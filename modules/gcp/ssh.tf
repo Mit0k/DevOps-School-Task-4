7d90ef61032add7633544bb7f097867bd858e0de
Alex Mitok <mitok.alexander@gmail.com>
2021-12-07 17:38:08 +0200

    Update ssh.tf

--- modules/gcp/ssh.tf
+++ modules/gcp/ssh.tf
@@ -1,7 +1,6 @@
 resource "null_resource" "ssh-generator" {
   triggers = { always_run =timestamp() }
   provisioner "local-exec" {
-    #command = "ssh-keygen -t rsa -f ${var.ssh_path} -C ${var.gcp_instance_username} -N ''"
     command = "scripts/ssh-keygen.sh ${var.ssh_path} ${var.gcp_instance_username}"
   }
 }
